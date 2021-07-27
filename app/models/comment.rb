4db7cbbe45aa59c97ba5ffee92e09b4c99f67b0b
Michael Antis <antis.michael@gmail.com>
2021-07-26 20:55:36 -0400

    add a dash of styling.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :ride
   belongs_to :commenter, class_name: :User, foreign_key: :user_id
+  #validates :user_id, :ride_id, presence: true
 end
--- spec/system/user_creates_a_new_ride_spec.rb
+++ spec/system/user_creates_a_new_ride_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature 'User creates a new ride' do 
+
   scenario 'it is saved to the database' do
-    
     bill = create(:user)
     sign_in bill
     visit root_path
@@ -12,6 +12,18 @@ RSpec.feature 'User creates a new ride' do
     click_on 'Submit'
     expect(page).to have_css 'a', text: 'Sunset Ride'
   end
+
+  scenario 'it is not saved to the database' do
+    bill = create(:user)
+    sign_in bill 
+    visit root_path
+    click_link 'Add new ride'
+    fill_in 'Title', with: 'Dangerous ride'
+    fill_in 'Date', with: 'F00342'
+    click_on 'Submit'
+    expect(page).to have_content 'span', text: 'Date can\'t be blan '
+  end
+
 end
 
 
